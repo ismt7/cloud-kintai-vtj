2a8e01cb657491ccbc059e57193d5cee85c0674c
Tatsuya Ishimoto <ishimoto@virtualtech.jp>
2023-01-23 19:57:07 +0900

    refactoring

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,24 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import Attendance from './Attendance';
-import Top from './Top';
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Attendance from "./Attendance";
+import Top from "./Top";
 
-test('TOP画面への遷移', () => {
-    const route = '/top'
-    render(
-        <MemoryRouter initialEntries={[route]}>
-            <Top />
-        </MemoryRouter>
-    );
-    expect(screen.getByText('Top')).toBeInTheDocument();
-})
+test("TOP画面への遷移", () => {
+  const route = "/top";
+  render(
+    <MemoryRouter initialEntries={[route]}>
+      <Top />
+    </MemoryRouter>
+  );
+  expect(screen.getByText("Top")).toBeInTheDocument();
+});
 
-test('勤怠打刻画面への遷移', () => {
-    const route = '/attendance'
-    render(
-        <MemoryRouter initialEntries={[route]}>
-            <Attendance />
-        </MemoryRouter>
-    );
-    expect(screen.getByText('勤怠打刻画面')).toBeInTheDocument();
-})
+test("勤怠打刻画面への遷移", () => {
+  const route = "/attendance";
+  render(
+    <MemoryRouter initialEntries={[route]}>
+      <Attendance />
+    </MemoryRouter>
+  );
+  expect(screen.getByText("勤怠打刻画面")).toBeInTheDocument();
+});
