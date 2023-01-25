b15a9a817f4407b903ee89c0652f6601c7fa44a7
Tatsuya Ishimoto <ishimoto@virtualtech.jp>
2023-01-25 16:41:29 +0900

    add button component

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,41 +1,76 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Button } from './Button';
+import Button from "./Button";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    // backgroundColor: { control: "color" },
   },
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+export const Save = Template.bind({});
+Save.storyName = "保存";
+Save.args = {
+  color: "primary",
+  label: "保存",
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const Cancel = Template.bind({});
+Cancel.storyName = "キャンセル";
+Cancel.args = {
+  color: "cancel",
+  label: "キャンセル",
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const ClockIn = Template.bind({});
+ClockIn.storyName = "勤務開始";
+ClockIn.args = {
+  variant: "outlined",
+  color: "clock_in",
+  size: "large",
+  label: "勤務開始",
+};
+
+export const ClockOut = Template.bind({});
+ClockOut.storyName = "勤務終了";
+ClockOut.args = {
+  variant: "outlined",
+  color: "clock_out",
+  size: "large",
+  label: "勤務終了",
+};
+
+export const RestStart = Template.bind({});
+RestStart.storyName = "休憩開始";
+RestStart.args = {
+  color: "rest",
+  label: "休憩開始",
+};
+
+export const RestEnd = Template.bind({});
+RestEnd.storyName = "休憩終了";
+RestEnd.args = {
+  color: "rest",
+  label: "休憩終了",
+};
+
+export const EditSmall = Template.bind({});
+EditSmall.storyName = "編集(一覧用)";
+EditSmall.args = {
+  color: "primary",
+  size: "small",
+  label: "編集",
 };
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,48 +1,251 @@
-import React from 'react';
-import './button.css';
+import { Button as MuiButton, createTheme, ThemeProvider } from "@mui/material";
+import "./button.css";
+
+type Color =
+  | "inherit"
+  | "primary"
+  | "secondary"
+  | "success"
+  | "error"
+  | "info"
+  | "warning"
+  | "cancel"
+  | "rest"
+  | "clock_in"
+  | "clock_out";
+type Variant = "text" | "outlined" | "contained";
+type Size = "small" | "medium" | "large";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
+  color?: Color;
+  variant?: Variant;
+  disabled?: boolean;
+  size?: Size;
+  label?: string;
+  width?: string;
+  height?: string;
+  borderRadius?: string;
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
+declare module "@mui/material/styles" {
+  interface Palette {
+    cancel: Palette["error"];
+    rest: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    cancel?: PaletteOptions["error"];
+    rest?: PaletteOptions["primary"];
+    clock_in?: PaletteOptions["primary"];
+    clock_out?: PaletteOptions["error"];
+  }
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    cancel: true;
+    rest: true;
+    clock_in: true;
+    clock_out: true;
+  }
+
+  interface ButtonPropsVariantOverrides {
+    cancel: true;
+    rest: true;
+    clock_in: true;
+    clock_out: true;
+  }
+}
+
+const palette = {
+  primary: { main: "#0FA85E", contrastText: "#fff" },
+  secondary: { main: "#B33D47", contrastText: "#fff" },
+  success: { main: "#0FA85E", contrastText: "#fff" },
+  error: { main: "#B33D47", contrastText: "#fff" },
+  warning: { main: "#B33D47", contrastText: "#fff" },
+  cancel: { main: "#B33D47", contrastText: "#fff" },
+  rest: { main: "#2ACEDB", contrastText: "#fff" },
+  clock_in: { main: "#0FA85E", contrastText: "#fff" },
+  clock_out: { main: "#B33D47", contrastText: "#fff" },
+};
+
+const theme = createTheme({
+  palette,
+  typography: {
+    fontFamily: ["Noto Sans JP"].join(","),
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Noto Sans JP';
+          font-style: bold;
+          font-weight: 700;
+        }`,
+    },
+    MuiButton: {
+      variants: [
+        {
+          props: { variant: "contained", color: "primary" },
+          style: {
+            backgroundColor: palette.primary.main,
+            color: palette.primary.contrastText,
+            boxShadow: "none",
+            border: `3px solid ${palette.primary.main}`,
+            "&:hover": {
+              backgroundColor: palette.primary.contrastText,
+              color: palette.primary.main,
+              border: `3px solid ${palette.primary.main}`,
+              boxShadow: "none",
+            },
+            "&:disabled": {
+              border: "3px solid",
+            },
+          },
+        },
+        {
+          props: { variant: "contained", color: "cancel" },
+          style: {
+            backgroundColor: palette.cancel.main,
+            color: palette.cancel.contrastText,
+            boxShadow: "none",
+            border: `3px solid ${palette.cancel.main}`,
+            "&:hover": {
+              backgroundColor: palette.cancel.contrastText,
+              color: palette.cancel.main,
+              border: "3px solid",
+              boxShadow: "none",
+            },
+          },
+        },
+        {
+          props: { variant: "contained", color: "rest" },
+          style: {
+            backgroundColor: palette.rest.main,
+            color: palette.rest.contrastText,
+            boxShadow: "none",
+            border: `3px solid ${palette.rest.main}`,
+            "&:hover": {
+              backgroundColor: palette.rest.contrastText,
+              color: palette.rest.main,
+              border: `3px solid ${palette.rest.main}`,
+              boxShadow: "none",
+            },
+            "&:disabled": {
+              border: "3px solid",
+            },
+          },
+        },
+        {
+          props: { variant: "outlined", color: "clock_in", size: "large" },
+          style: {
+            backgroundColor: palette.clock_in.contrastText,
+            color: palette.clock_in.main,
+            boxShadow: "none",
+            border: `3px solid ${palette.clock_in.main}`,
+            borderRadius: "50%",
+            width: "120px",
+            height: "120px",
+            "&:hover": {
+              backgroundColor: palette.clock_in.main,
+              color: palette.clock_in.contrastText,
+              border: `3px solid ${palette.clock_in.main}`,
+              boxShadow: "none",
+            },
+            "&:disabled": {
+              border: "3px solid",
+            },
+          },
+        },
+        {
+          props: { variant: "contained", color: "clock_in", size: "large" },
+          style: {
+            backgroundColor: palette.clock_in.main,
+            color: palette.clock_in.contrastText,
+            boxShadow: "none",
+            border: `3px solid ${palette.clock_in.main}`,
+            borderRadius: "50%",
+            width: "120px",
+            height: "120px",
+            "&:hover": {
+              backgroundColor: palette.clock_in.contrastText,
+              color: palette.clock_in.main,
+              border: `3px solid ${palette.clock_in.main}`,
+              boxShadow: "none",
+            },
+            "&:disabled": {
+              border: "3px solid",
+            },
+          },
+        },
+        {
+          props: { variant: "outlined", color: "clock_out", size: "large" },
+          style: {
+            backgroundColor: palette.clock_out.contrastText,
+            color: palette.clock_out.main,
+            boxShadow: "none",
+            border: `3px solid ${palette.clock_out.main}`,
+            borderRadius: "50%",
+            width: "120px",
+            height: "120px",
+            "&:hover": {
+              backgroundColor: palette.clock_out.main,
+              color: palette.clock_out.contrastText,
+              border: `3px solid ${palette.clock_out.main}`,
+              boxShadow: "none",
+            },
+            "&:disabled": {
+              border: "3px solid",
+            },
+          },
+        },
+        {
+          props: { variant: "contained", color: "clock_out", size: "large" },
+          style: {
+            backgroundColor: palette.clock_out.main,
+            color: palette.clock_out.contrastText,
+            boxShadow: "none",
+            border: `3px solid ${palette.clock_out.main}`,
+            borderRadius: "50%",
+            width: "120px",
+            height: "120px",
+            "&:hover": {
+              backgroundColor: palette.clock_out.contrastText,
+              color: palette.clock_out.main,
+              border: `3px solid ${palette.clock_out.main}`,
+              boxShadow: "none",
+            },
+            "&:disabled": {
+              border: "3px solid",
+            },
+          },
+        },
+      ],
+    },
+  },
+});
+
+const Button = ({
+  color = "primary",
+  variant = "contained",
+  disabled = false,
+  size = "medium",
+  label = "Button",
+
   ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
+}: ButtonProps) => (
+  <ThemeProvider theme={theme}>
+    <MuiButton
+      color={color}
+      variant={variant}
+      disabled={disabled}
+      size={size}
+      style={{ ...props }}
     >
       {label}
-    </button>
-  );
-};
+    </MuiButton>
+  </ThemeProvider>
+);
+
+export default Button;
