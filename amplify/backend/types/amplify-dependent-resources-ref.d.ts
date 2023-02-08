4ca3eb0438a3f2744403338e16561e37998833a1
Tatsuya Ishimoto <ishimoto@virtualtech.jp>
2023-02-08 15:39:22 +0900

    add amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "garakufrontend5824eef0": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,32 @@
+import { useEffect } from "react";
+import { Authenticator, useAuthenticator, View } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useLocation, useNavigate } from "react-router";
+import awsConfig from "../aws-exports";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(awsConfig);
+
 function Login() {
-  return <h1>Login</h1>;
+  const { route } = useAuthenticator((context) => [context.route]);
+  const location = useLocation();
+  const navigate = useNavigate();
+  // eslint-disable-next-line max-len
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const from = location.state?.from || "/";
+
+  useEffect(() => {
+    if (route === "authenticated") {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      navigate(from, { replace: true });
+    }
+  }, [route, navigate, from]);
+
+  return (
+    <View className="auth-wrapper">
+      <Authenticator></Authenticator>
+    </View>
+  );
 }
 export default Login;
