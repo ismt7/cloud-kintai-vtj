0065f3fcd83538ab350347a5e8fb6cf7bde79bb9
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2023-05-31 06:52:46 +0000

    add custom .bashrc

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,16 @@
 set -e
 
 # ========================================
+#  Add Custom settings in .bashrc
+# ========================================
+BASHRC_PATH=/home/node/.bashrc
+cat <<EOL >> $BASHRC_PATH
+
+# Custom settings
+source /workspaces/garaku-frontend/.devcontainer/.bashrc.custom
+EOL
+
+# ========================================
 #  Application Package install
 # ========================================
 make install
