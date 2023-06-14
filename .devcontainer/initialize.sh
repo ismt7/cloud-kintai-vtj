e574861f9a3dff6c2e3c7ee459c918b34033a395
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2023-06-14 04:40:57 +0000

    Add initialize script for devcontainer

---
+++ .devcontainer/initialize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# ========================================
+#  Create devcontainer.env file
+# ========================================
+
+# ファイルが存在しない場合は作成する
+if [ ! -e .devcontainer/devcontainer.env ]; then
+  cp .devcontainer/devcontainer.env.example .devcontainer/devcontainer.env
+fi
+
+# ========================================
+#  Create .env file
+# ========================================
+
+# ファイルが存在しない場合は作成する
+if [ ! -e .env ]; then
+  cp .env.example .env
+fi
