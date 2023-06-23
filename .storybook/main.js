ef83f46622305cbd1dcd3248fb4c76017e85e5bd
ismt7 <ishimoto@virtualtech.jp>
2023-06-23 02:54:26 +0000

    Update Storybook configuration for source path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
-  "stories": ["../src/stories/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)", "../src/components/**/*.stories.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  // "stories": ["../src/stories/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)", "../src/components/**/*.stories.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  "stories": ["../src"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
   "framework": {
     name: "@storybook/react-webpack5",
