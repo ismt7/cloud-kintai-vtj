5638f17c5bb4269c48bd84205ddc7db7e8919049
ismt7 <ishimoto@virtualtech.jp>
2023-07-11 05:13:02 +0000

    update test code

--- src/__tests__/components/attendance_editor/AttendanceEditor.test.tsx
+++ src/__tests__/components/attendance_editor/AttendanceEditor.test.tsx
@@ -1,10 +1,41 @@
+import { ThemeProvider } from "@mui/material";
 import { render } from "@testing-library/react";
-
+import dayjs from "dayjs";
+import { Provider } from "react-redux";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import AttendanceEditor from "../../../components/attendance_editor/AttendanceEditor";
+import GetStoreMock from "../../../components/attendance_editor/mocks/MockReducer";
+
+import { theme } from "../../../lib/theme";
 
 describe("AttendanceEditor Component(", () => {
-  test.concurrent("レイアウト崩れが発生していないか(ビジュアルリグレッション)", () => {
-    const { asFragment } = render(<AttendanceEditor />);
-    expect(asFragment()).toMatchSnapshot();
-  });
+  test.concurrent(
+    "レイアウト崩れが発生していないか(ビジュアルリグレッション)",
+    () => {
+      // Arrange: テスト準備
+      const now = dayjs();
+      const targetWorkDate = now.format("YYYYMMDD");
+
+      // Act: テスト実行
+      const { asFragment } = render(
+        <Provider store={GetStoreMock()}>
+          <MemoryRouter
+            initialEntries={[`/admin/attendances/edit/${targetWorkDate}/999`]}
+          >
+            <ThemeProvider theme={theme}>
+              <Routes>
+                <Route
+                  path="/admin/attendances/edit/:targetWorkDate/:targetStaffId"
+                  element={<AttendanceEditor />}
+                />
+              </Routes>
+            </ThemeProvider>
+          </MemoryRouter>
+        </Provider>
+      );
+
+      // Assert: 検証
+      expect(asFragment()).toMatchSnapshot();
+    }
+  );
 });
