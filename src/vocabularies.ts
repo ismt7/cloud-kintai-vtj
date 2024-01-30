1df8582c413d3adf664cac888aea078ace75f522
ismt7 <ishimoto@virtualtech.jp>
2024-01-30 06:03:32 +0000

    Add Japanese language support and vocabularies

---
+++ src/vocabularies.ts
@@ -0,0 +1,21 @@
+const vocabularies = {
+  ja: {
+    Email: "メールアドレス",
+    "Enter your Email": "メールアドレスを入力",
+    Password: "パスワード",
+    "Enter your Password": "パスワードを入力",
+    "Sign in": "ログイン",
+    "Forgot your password?": "パスワードを忘れた場合",
+    "Change Password": "パスワード変更",
+    "Confirm Password": "パスワードの確認",
+    "Back to Sign In": "ログインに戻る",
+    "Password must have at least 8 characters":
+      "パスワードは8文字以上である必要があります",
+    "Please confirm your password": "パスワードを確認してください",
+    "Invalid session for the user, session is expired.":
+      "セッションが切れています。再度アクセスし直してからログインしてください。",
+    "Signing in": "ログイン中",
+  },
+};
+
+export default vocabularies;
