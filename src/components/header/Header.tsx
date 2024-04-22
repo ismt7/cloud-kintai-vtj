0be223cf2dd45cd2c0b55a8d5224301c9f6e2bbb
ismt7 <ishimoto@virtualtech.jp>
2024-04-22 02:45:24 +0000

    fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,24 @@
-import { Button, Container, Stack, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  Paper,
+  Stack,
+  styled,
+} from "@mui/material";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Unstable_Popup as BasePopup } from "@mui/base/Unstable_Popup";
 
 import DesktopMenu from "./DesktopMenu";
 import Logo from "./Logo";
 import MobileMenu from "./MobileMenu";
-import { CognitoUser } from "../../hooks/useCognitoUser";
 import { SignInOutButton } from "./SignInOutButton";
+import AppsIcon from "@mui/icons-material/Apps";
+import { theme } from "../../lib/theme";
+import { ClickAwayListener } from "@mui/base/ClickAwayListener";
+import { LinkGridItem } from "./LinkGridItem";
 
 export const SignOutButton = styled(Button)(({ theme }) => ({
   color: theme.palette.logout.contrastText,
@@ -59,9 +71,59 @@ export default function Header() {
           <Logo />
           <DesktopMenu pathName={pathName} />
           <MobileMenu pathName={pathName} />
+          <ExternalLinks />
           <SignInOutButton pathName={pathName} />
         </Stack>
       </Container>
     </header>
+  );
+}
+
+function ExternalLinks() {
+  const [anchor, setAnchor] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchor(anchor ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchor);
+  const id = open ? "external-links-popup" : undefined;
+
+  const handleClickAway = () => {
+    setAnchor(null);
+  };
+
+  return (
+    <>
+      <IconButton onClick={handleClick}>
+        <AppsIcon
+          sx={{
+            color: "white",
+          }}
+        />
+      </IconButton>
+      <BasePopup id={id} open={open} anchor={anchor}>
+        <ClickAwayListener onClickAway={handleClickAway}>
+          <Paper
+            elevation={3}
+            sx={{
+              width: "300px",
+              height: "400px",
+              m: 2,
+              p: 2,
+              border: `5px solid ${theme.palette.primary.main}`,
+            }}
+          >
+            <Grid container spacing={1}>
+              <LinkGridItem
+                url="http://ginjiro.office.begi.net:3021/"
+                title="交通費申請"
+                iconType="train"
+              />
+            </Grid>
+          </Paper>
+        </ClickAwayListener>
+      </BasePopup>
+    </>
   );
 }
