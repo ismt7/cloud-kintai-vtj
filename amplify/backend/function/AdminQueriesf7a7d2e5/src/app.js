8fe674323c1a3f41e1105087f1a263f9d6a846a2
ismt7 <ishimoto@virtualtech.jp>
2024-05-17 06:40:51 +0000

    fix

--- amplify/backend/function/AdminQueriesf7a7d2e5/src/app.js
+++ amplify/backend/function/AdminQueriesf7a7d2e5/src/app.js
@@ -44,6 +44,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "OPTIONS,POST,GET");
   next();
 });
 
