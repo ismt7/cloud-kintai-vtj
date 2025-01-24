cea51b6906adc83b446acef5516bbab99cd05fa6
ismt7 <ishimoto@virtualtech.jp>
2025-01-24 02:21:02 +0000

    Improve error handling

--- src/components/AttendanceDailyList/AttendanceDailyList.tsx
+++ src/components/AttendanceDailyList/AttendanceDailyList.tsx
@@ -10,29 +10,57 @@ import {
   TableRow,
 } from "@mui/material";
 import dayjs from "dayjs";
-import { useEffect } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 import { useParams } from "react-router-dom";
 
+import { useAppDispatchV2 } from "@/app/hooks";
 import MoveDateItem from "@/components/AttendanceDailyList/MoveDateItem";
+import * as MESSAGE_CODE from "@/errors";
 import { AttendanceDate } from "@/lib/AttendanceDate";
+import { setSnackbarError } from "@/lib/reducers/snackbarReducer";
 
-import useAttendanceDaily from "../../hooks/useAttendanceDaily/useAttendanceDaily";
+import useAttendanceDaily, {
+  AttendanceDaily,
+} from "../../hooks/useAttendanceDaily/useAttendanceDaily";
 import { ActionsTableCell } from "./ActionsTableCell";
 import { EndTimeTableCell } from "./EndTimeTableCell";
 import { StartTimeTableCell } from "./StartTimeTableCell";
 
 export default function AttendanceDailyList() {
   const { targetWorkDate } = useParams();
-  const { attendanceDailyList, fetchAllByWorkDate } = useAttendanceDaily();
+  const { attendanceDailyList, error } = useAttendanceDaily();
   const today = dayjs().format(AttendanceDate.QueryParamFormat);
+  const dispatch = useAppDispatchV2();
 
   useEffect(() => {
-    if (!targetWorkDate) {
-      return;
+    if (error) {
+      dispatch(setSnackbarError(MESSAGE_CODE.E00001));
+      console.error(error);
     }
+  }, [error]);
 
-    fetchAllByWorkDate(dayjs(targetWorkDate).format(AttendanceDate.DataFormat));
-  }, [targetWorkDate]);
+  const sortedAttendanceList = useMemo(() => {
+    return attendanceDailyList.sort((a, b) => {
+      const aSortKey = a.sortKey || "";
+      const bSortKey = b.sortKey || "";
+      return aSortKey.localeCompare(bSortKey);
+    });
+  }, [attendanceDailyList]);
+
+  const renderSummaryMessage = useCallback((row: AttendanceDaily) => {
+    if (!row.attendance) return "";
+    const { paidHolidayFlag, substituteHolidayDate, remarks } = row.attendance;
+    const isSubstituteHoliday = substituteHolidayDate
+      ? dayjs(substituteHolidayDate).isValid()
+      : false;
+
+    const summaryMessage = [];
+    if (paidHolidayFlag) summaryMessage.push("有給休暇");
+    if (isSubstituteHoliday) summaryMessage.push("振替休日");
+    if (remarks) summaryMessage.push(remarks);
+
+    return summaryMessage.join(" ");
+  }, []);
 
   return (
     <Stack direction="column" spacing={1}>
@@ -56,41 +84,16 @@ export default function AttendanceDailyList() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {attendanceDailyList
-              .sort((a, b) => {
-                const aSortKey = a.sortKey || "";
-                const bSortKey = b.sortKey || "";
-                return aSortKey.localeCompare(bSortKey);
-              })
-              .map((row, index) => (
-                <TableRow key={index} className="attendance-row">
-                  <ActionsTableCell row={row} />
-                  <TableCell>{`${row.familyName} ${row.givenName}`}</TableCell>
-                  <StartTimeTableCell row={row} />
-                  <EndTimeTableCell row={row} />
-                  <TableCell>
-                    {(() => {
-                      if (!row.attendance) return "";
-                      const {
-                        paidHolidayFlag,
-                        substituteHolidayDate,
-                        remarks,
-                      } = row.attendance;
-                      const isSubstituteHoliday = substituteHolidayDate
-                        ? dayjs(substituteHolidayDate).isValid()
-                        : false;
-
-                      const summaryMessage = [];
-                      if (paidHolidayFlag) summaryMessage.push("有給休暇");
-                      if (isSubstituteHoliday) summaryMessage.push("振替休日");
-                      if (remarks) summaryMessage.push(remarks);
-
-                      return summaryMessage.join(" ");
-                    })()}
-                  </TableCell>
-                  <TableCell sx={{ whiteSpace: "nowrap" }} />
-                </TableRow>
-              ))}
+            {sortedAttendanceList.map((row, index) => (
+              <TableRow key={index} className="attendance-row">
+                <ActionsTableCell row={row} />
+                <TableCell>{`${row.familyName} ${row.givenName}`}</TableCell>
+                <StartTimeTableCell row={row} />
+                <EndTimeTableCell row={row} />
+                <TableCell>{renderSummaryMessage(row)}</TableCell>
+                <TableCell sx={{ whiteSpace: "nowrap" }} />
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
