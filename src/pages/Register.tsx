30f4d803a48a184c53e2ac0dda5a2021b2bc4ba1
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-04-18 14:27:31 +0900

    add warning alert for disabled register functionality

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,21 @@
-import { Box } from "@mui/material";
+import { Box, Alert, Container } from "@mui/material";
 
 import TimeRecorder from "../components/time_recorder/TimeRecorder";
 
 export default function Register() {
+  const isRegisterDisabled =
+    import.meta.env.VITE_STANDARD_REGISTER_DISABLE === "true";
+
+  if (isRegisterDisabled) {
+    return (
+      <Container>
+        <Box sx={{ mt: 4, textAlign: "center" }}>
+          <Alert severity="warning">現在、こちらの機能は使用できません</Alert>
+        </Box>
+      </Container>
+    );
+  }
+
   return (
     <Box
       sx={{
