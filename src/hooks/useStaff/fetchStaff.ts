8fa464befcd52ba66d7d40372b6935feaa455f6e
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-05-13 16:38:24 +0900

    refactor fetchStaff and fetchStaffs functions to simplify data retrieval and error handling

--- src/hooks/useStaff/fetchStaff.ts
+++ src/hooks/useStaff/fetchStaff.ts
@@ -1,46 +1,31 @@
 import { GraphQLResult } from "@aws-amplify/api";
 import { API } from "aws-amplify";
 
-import { ListStaffQuery, Staff } from "../../API";
-import { listStaff } from "../../graphql/queries";
+import { ListStaffQuery, Staff, StaffByCognitoUserIdQuery } from "../../API";
+import { listStaff, staffByCognitoUserId } from "../../graphql/queries";
 
 export default async function fetchStaff(
   cognitoUserId: Staff["cognitoUserId"]
 ) {
-  const staffs: Staff[] = [];
-  let nextToken: string | null = null;
-  const isLooping = true;
-  while (isLooping) {
-    const response = (await API.graphql({
-      query: listStaff,
-      variables: {
-        filter: { cognitoUserId: { eq: cognitoUserId } },
-        nextToken,
-      },
-      authMode: "AMAZON_COGNITO_USER_POOLS",
-    })) as GraphQLResult<ListStaffQuery>;
+  const response = (await API.graphql({
+    query: staffByCognitoUserId,
+    variables: {
+      cognitoUserId,
+    },
+    authMode: "AMAZON_COGNITO_USER_POOLS",
+  })) as GraphQLResult<StaffByCognitoUserIdQuery>;
 
-    if (response.errors) {
-      throw new Error(response.errors[0].message);
-    }
-
-    if (!response.data?.listStaff?.items) {
-      throw new Error("スタッフが見つかりませんでした。");
-    }
-
-    staffs.push(
-      ...response.data.listStaff.items.filter(
-        (item): item is NonNullable<typeof item> => item !== null
-      )
-    );
-
-    if (response.data.listStaff.nextToken) {
-      nextToken = response.data.listStaff.nextToken;
-      continue;
-    }
-
-    break;
+  if (response.errors) {
+    throw new Error(response.errors[0].message);
   }
+
+  if (!response.data?.staffByCognitoUserId?.items) {
+    throw new Error("スタッフが見つかりませんでした。");
+  }
+
+  const staffs = response.data.staffByCognitoUserId.items.filter(
+    (item): item is NonNullable<typeof item> => item !== null
+  );
 
   return staffs[0];
 }
