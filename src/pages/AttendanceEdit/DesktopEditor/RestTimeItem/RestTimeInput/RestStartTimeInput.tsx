8f5a4efa3685e1f04ae47d719c0ac94d8de6d873
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-06-10 16:11:18 +0900

    add data-testid attributes to RestStartTimeInput for testing purposes

--- src/pages/AttendanceEdit/DesktopEditor/RestTimeItem/RestTimeInput/RestStartTimeInput.tsx
+++ src/pages/AttendanceEdit/DesktopEditor/RestTimeItem/RestTimeInput/RestStartTimeInput.tsx
@@ -1,5 +1,5 @@
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
-import { Box, Chip, CircularProgress, Stack } from "@mui/material";
+import { Box, Chip, Stack } from "@mui/material";
 import { renderTimeViewClock, TimePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 import { useContext } from "react";
@@ -43,7 +43,10 @@ export default function RestStartTimeInput({
               minutes: renderTimeViewClock,
             }}
             slotProps={{
-              textField: { size: "small" },
+              textField: {
+                size: "small",
+                inputProps: { "data-testid": `rest-start-time-input-${index}` },
+              },
             }}
             onChange={(newStartTime) => {
               if (!newStartTime) return null;
@@ -71,6 +74,7 @@ export default function RestStartTimeInput({
           color="success"
           icon={<AddCircleOutlineOutlinedIcon fontSize="small" />}
           disabled={changeRequests.length > 0}
+          data-testid={`rest-lunch-chip-${index}`}
           onClick={() => {
             const startTime = dayjs(
               `${workDate.format("YYYY-MM-DD")} ${lunchRestStartTime}`
